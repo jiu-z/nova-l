5227cec6d666907daa483925b968afc1153735ee
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-28 16:16:44 +0900

    description

--- 3-hands-on/main.tf
+++ 3-hands-on/main.tf
@@ -12,29 +12,8 @@ provider "google" {
   region = var.region
 }
 
-# resource "google_compute_subnetwork" "ca-subnetwork" {
-#   name          = "ca-subnetwork"
-#   ip_cidr_range = var.subnetwork_range
-#   region        = var.region
-#   network       = google_compute_network.ca-network.id
-# }
-
 resource "google_compute_network" "ca-network" {
   name                    = "ca-network"
   auto_create_subnetworks = var.auto_create_subnetworks
 }
 
-# resource "google_compute_instance" "instance" {
-#   name         = "ca-lab"
-#   machine_type = var.machine_type
-#   zone         = var.zone
-#   boot_disk {
-#     initialize_params {
-#       image = var.image
-#     }
-#   }
-#   network_interface {
-#     subnetwork = google_compute_subnetwork.ca-subnetwork.id
-#     access_config {}
-#   }
-# }
