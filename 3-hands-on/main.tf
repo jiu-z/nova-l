37341618006bc2b27afefaa164e3b28bebea7f3c
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-28 17:52:04 +0900

    update

--- 3-hands-on/main.tf
+++ 3-hands-on/main.tf
@@ -71,37 +71,37 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-# resource "google_cloud_run_service" "default" {
-#   name     = "cloudrun-srv"
-#   location = "us-central1"
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
 
-#   template {
-#     spec {
-#       containers {
-#         # image = "us-docker.pkg.dev/cloudrun/container/hello"
-#         # image = "us-central1-docker.pkg.dev/era-ax/infra/init:latest"
-#         image = var.container_image
-#       }
-#     }
-#   }
-# }
+  template {
+    spec {
+      containers {
+        # image = "us-docker.pkg.dev/cloudrun/container/hello"
+        # image = "us-central1-docker.pkg.dev/era-ax/infra/init:latest"
+        image = var.container_image
+      }
+    }
+  }
+}
 
-# data "google_iam_policy" "noauthc" {
-#   binding {
-#     role = "roles/run.invoker"
-#     members = [
-#       "allUsers",
-#     ]
-#   }
-# }
+data "google_iam_policy" "noauthc" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
 
-# resource "google_cloud_run_service_iam_policy" "noauthc" {
-#   location    = google_cloud_run_service.default.location
-#   project     = google_cloud_run_service.default.project
-#   service     = google_cloud_run_service.default.name
+resource "google_cloud_run_service_iam_policy" "noauthc" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
 
-#   policy_data = data.google_iam_policy.noauthc.policy_data
-# }
+  policy_data = data.google_iam_policy.noauthc.policy_data
+}
 
 
 # resource "google_compute_region_network_endpoint_group" "webserver" {
