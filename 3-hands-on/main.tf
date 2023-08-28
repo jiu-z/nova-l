fb1a6cdfa402da3889ee024f716ab1dd0e6fabad
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-28 18:10:57 +0900

    update

--- 3-hands-on/main.tf
+++ 3-hands-on/main.tf
@@ -71,75 +71,29 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
-# resource "google_cloud_run_service" "default" {
-#   name     = "cloudrun-srv"
-#   location = "us-central1"
+resource "google_compute_region_network_endpoint_group" "node_group" {
+  name                  = "${var.service_name}-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  project               = var.project_id
+  cloud_run {
+    service = google_cloud_run_service.webserver.name
+  }
+}
 
-#   template {
-#     spec {
-#       containers {
-#         image = var.container_image
-#       }
-#     }
-#   }
-#   metadata {
-#     annotations = {
-#       "run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
-#     }
-#   }
-#     lifecycle {
-#     ignore_changes = [
-#       metadata.0.annotations["run.googleapis.com/client-name"],
-#       metadata.0.annotations["run.googleapis.com/client-version"],
-#       template[0].spec[0].containers[0].image,
-#       template[0].spec[0].containers[0].env
-#     ]
-#   }
-
-#   autogenerate_revision_name = true
-# }
-
-# data "google_iam_policy" "noauthc" {
-#   binding {
-#     role = "roles/run.invoker"
-#     members = [
-#       "allUsers",
-#     ]
-#   }
-# }
-
-# resource "google_cloud_run_service_iam_policy" "noauthc" {
-#   location    = google_cloud_run_service.default.location
-#   project     = google_cloud_run_service.default.project
-#   service     = google_cloud_run_service.default.name
-
-#   policy_data = data.google_iam_policy.noauthc.policy_data
-# }
-
-
-# resource "google_compute_region_network_endpoint_group" "webserver" {
-#   name                  = "${var.service_name}-neg"
-#   network_endpoint_type = "SERVERLESS"
-#   region                = var.region
-#   project               = var.project_id
-#   cloud_run {
-#     service = google_cloud_run_service.webserver.name
-#   }
-# }
-
-# resource "google_compute_backend_service" "webserver" {
-#   name                  = google_cloud_run_service.webserver.name
-#   protocol              = "HTTP"
-#   load_balancing_scheme = "EXTERNAL_MANAGED"
-#   security_policy       = var.security_policy
-#   backend {
-#     group = google_compute_region_network_endpoint_group.webserver.id
-#   }
-#   log_config {
-#     enable      = true
-#     sample_rate = 1
-#   }
-# }
+resource "google_compute_backend_service" "webserver" {
+  name                  = google_cloud_run_service.webserver.name
+  protocol              = "HTTP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  security_policy       = var.security_policy
+  backend {
+    group = google_compute_region_network_endpoint_group.node_group.id
+  }
+  log_config {
+    enable      = true
+    sample_rate = 1
+  }
+}
 
 ##Security
 # resource "google_compute_security_policy" "default_policy" {
