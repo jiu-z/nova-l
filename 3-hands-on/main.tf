65a546656baaf0526bc00c9441f737593d42fde0
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-28 17:44:33 +0900

    update

--- 3-hands-on/main.tf
+++ 3-hands-on/main.tf
@@ -71,6 +71,37 @@ resource "google_cloud_run_service_iam_policy" "noauth" {
   policy_data = data.google_iam_policy.noauth.policy_data
 }
 
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "noauthc" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauthc" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauthc.policy_data
+}
+
+
 # resource "google_compute_region_network_endpoint_group" "webserver" {
 #   name                  = "${var.service_name}-neg"
 #   network_endpoint_type = "SERVERLESS"
