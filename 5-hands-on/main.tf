ac352b2e28684ccdd1fd1b3f0c73262cb594dc96
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-31 11:07:26 +0900

    sd

--- 5-hands-on/main.tf
+++ 5-hands-on/main.tf
@@ -52,53 +52,53 @@ resource "google_firebase_project_location" "basic" {
     location_id = "us-central"
 }
 
-resource "google_firebase_android_app" "default" {
-  provider = google-beta
-  project = var.project_id
-  display_name = "android nova-kk"
-  package_name = "android.nova-kk.app"
-  sha1_hashes = ["2145bdf698b8715039bd0e83f2069bed435ac21c"]
-  sha256_hashes = ["2145bdf698b8715039bd0e83f2069bed435ac21ca1b2c3d4e5f6123456789abc"]
-  api_key_id = google_apikeys_key.android.uid
-}
+# resource "google_firebase_android_app" "default" {
+#   provider = google-beta
+#   project = var.project_id
+#   display_name = "android nova-kk"
+#   package_name = "android.nova-kk.app"
+#   sha1_hashes = ["2145bdf698b8715039bd0e83f2069bed435ac21c"]
+#   sha256_hashes = ["2145bdf698b8715039bd0e83f2069bed435ac21ca1b2c3d4e5f6123456789abc"]
+#   api_key_id = google_apikeys_key.android.uid
+# }
 
-resource "google_apikeys_key" "android" {
-  provider = google-beta
+# resource "google_apikeys_key" "android" {
+#   provider = google-beta
 
-  name         = "api-key"
-  display_name = "Display Name"
-  project = var.project_id
+#   name         = "api-key"
+#   display_name = "Display Name"
+#   project = var.project_id
 
-  restrictions {
-    android_key_restrictions {
-      allowed_applications {
-        package_name     = "android.nova-kk.app"
-        sha1_fingerprint = "2145bdf698b8715039bd0e83f2069bed435ac21c"
-      }
-    }
-  }
-}
+#   restrictions {
+#     android_key_restrictions {
+#       allowed_applications {
+#         package_name     = "android.nova-kk.app"
+#         sha1_fingerprint = "2145bdf698b8715039bd0e83f2069bed435ac21c"
+#       }
+#     }
+#   }
+# }
 
-resource "google_firebase_apple_app" "full" {
-  provider = google-beta
-  project = var.project_id
-  display_name = "nova-kk Full"
-  bundle_id = "apple.app.nova-kk"
-  app_store_id = "nova-kk"
-  team_id = "9987654321"
-  api_key_id = google_apikeys_key.apple.uid
-}
+# resource "google_firebase_apple_app" "full" {
+#   provider = google-beta
+#   project = var.project_id
+#   display_name = "nova-kk Full"
+#   bundle_id = "apple.app.nova-kk"
+#   app_store_id = "nova-kk"
+#   team_id = "9987654321"
+#   api_key_id = google_apikeys_key.apple.uid
+# }
 
-resource "google_apikeys_key" "apple" {
-  provider = google-beta
+# resource "google_apikeys_key" "apple" {
+#   provider = google-beta
 
-  name         = "api-key-apple"
-  display_name = "nova-kk Full"
-  project = var.project_id
+#   name         = "api-key-apple"
+#   display_name = "nova-kk Full"
+#   project = var.project_id
 
-  restrictions {
-    ios_key_restrictions {
-      allowed_bundle_ids = ["apple.app.nova-kk"]
-    }
-  }
-}
+#   restrictions {
+#     ios_key_restrictions {
+#       allowed_bundle_ids = ["apple.app.nova-kk"]
+#     }
+#   }
+# }
