83f7c10be647b5a668a9e589937f54b7852fd108
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-08-31 10:42:08 +0900

    sd

--- 5-hands-on/variables.tf
+++ 5-hands-on/variables.tf
@@ -1,16 +1,44 @@
+variable "auto_create_subnetworks" {
+    description = "Automatically create subnetworks"
+    type = string
+}
 variable "project_id" {
-  type = string
+    description = "Subnet IP address space"
+    type = string
 }
-
-variable "account_id_cloud_run" {
-  type = string
+variable "region" {
+    description = "Deployment Region"
+    type = string
 }
-
-variable "service_account_roles_permitions" {
-  type = list(object({
-    role_id     = string
-    title       = string
-    description = string
-    permissions = list(string)
-  }))
+variable "image"{
+    description = "Boot Disk Image"
+    type = string
+}
+variable "machine_type"{
+    description = "GCP Instance Type"
+    type = string
+}
+variable "zone" {
+    description = "Deployment Zone"
+    type = string
+}
+variable "service_name" {
+    description = "service_name"
+    type = string
+}
+variable "container_image" {
+    description = "container_image"
+    type = string
+}
+variable "policy_name" {
+    description = "policy_name"
+    type = string
+}
+variable "certificate_name" {
+    description = "certificate_name"
+    type = string
+}
+variable "lb_name" {
+    description = "lb_name"
+    type = string
 }
